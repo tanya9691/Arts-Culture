bc7f38033b33f02a1a44969eb88e34c20782ceb6
Tanuja <46497301+tanuja181@users.noreply.github.com>
2020-06-05 11:57:56 +0530

    updated

--- Major/urls.py
+++ Major/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+from django.conf.urls import include
+
+from Major import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include('app.urls')),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
