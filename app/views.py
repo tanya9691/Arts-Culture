43394237b88a4704894bc6cbe3e4eb2d197d795e
Tanuja <46497301+tanuja181@users.noreply.github.com>
2020-06-05 13:54:43 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -5,9 +5,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, View
 from .models import Item
-from rest_framework import generics
-from rest_framework.response import Response
-from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
+#from rest_framework import generics
+#from rest_framework.response import Response
+#from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 from .serializers import UserSerializer, UserLoginSerializer, UserLogoutSerializer
 
 import json
@@ -38,32 +38,32 @@ class ItemList(ListView):
         item =  Item.objects.all()
         return item
 
-class RegisterationView(generics.ListCreateAPIView):
-    # get method handler
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# class RegisterationView(generics.ListCreateAPIView):
+#     # get method handler
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
 
-class LoginView(generics.GenericAPIView):
-    # get method handler
-    queryset = User.objects.all()
-    serializer_class = UserLoginSerializer
+# class LoginView(generics.GenericAPIView):
+#     # get method handler
+#     queryset = User.objects.all()
+#     serializer_class = UserLoginSerializer
 
-    def post(self, request, *args, **kwargs):
-        serializer_class = UserLoginSerializer(data=request.data)
-        if serializer_class.is_valid(raise_exception=True):
-            return Response(serializer_class.data, status=HTTP_200_OK)
-        return Response(serializer_class.errors, status=HTTP_400_BAD_REQUEST)
+#     def post(self, request, *args, **kwargs):
+#         serializer_class = UserLoginSerializer(data=request.data)
+#         if serializer_class.is_valid(raise_exception=True):
+#             return Response(serializer_class.data, status=HTTP_200_OK)
+#         return Response(serializer_class.errors, status=HTTP_400_BAD_REQUEST)
 
 
-class LogoutView(generics.GenericAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserLogoutSerializer
+# class LogoutView(generics.GenericAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserLogoutSerializer
 
-    def post(self, request, *args, **kwargs):
-        serializer_class = UserLogoutSerializer(data=request.data)
-        if serializer_class.is_valid(raise_exception=True):
-            return Response(serializer_class.data, status=HTTP_200_OK)
-        return Response(serializer_class.errors, status=HTTP_400_BAD_REQUEST)
+#     def post(self, request, *args, **kwargs):
+#         serializer_class = UserLogoutSerializer(data=request.data)
+#         if serializer_class.is_valid(raise_exception=True):
+#             return Response(serializer_class.data, status=HTTP_200_OK)
+#         return Response(serializer_class.errors, status=HTTP_400_BAD_REQUEST)
 
 
 
