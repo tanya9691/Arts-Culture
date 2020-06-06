8a33322cabdb4f8cdb33776da35cd6c043d551b7
tanuja <tanujasahu.htb@gmail.com>
2020-06-06 23:41:19 +0530

    commit

--- Major/settings.py
+++ Major/settings.py
@@ -161,17 +161,17 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR,'app/static')
+#STATIC_ROOT = os.path.join(BASE_DIR,'app/static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__)) 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-#STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Extra places for collectstatic to find static files.
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'app/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 
 
