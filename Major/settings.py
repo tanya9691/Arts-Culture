122113f18f7ca43197a70cc1fd2ebde760bc39cb
tanuja <tanujasahu.htb@gmail.com>
2020-06-06 23:29:36 +0530

    commit

--- Major/settings.py
+++ Major/settings.py
@@ -30,7 +30,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','https://gentle-earth-67629.herokuapp.com/']
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1','https://artgrih.herokuapp.com/']
 
 
 # Application definition
