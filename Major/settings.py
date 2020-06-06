025595e8679c8fbbe02faad2ff4ad1bee18e86a0
tanuja <tanujasahu.htb@gmail.com>
2020-06-06 23:10:15 +0530

    commit

--- Major/settings.py
+++ Major/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '2v-8s7uqzen4i)7poh*54qglsjx@i6oz$j@wfxqufx4z2aq10q'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
