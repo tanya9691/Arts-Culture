32268d7a44dc084777b5cd5ccfdf18783aec783f
tanuja <tanujasahu.htb@gmail.com>
2020-06-07 10:31:11 +0530

    updated

--- Major/settings.py
+++ Major/settings.py
@@ -175,8 +175,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 
 
-#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
 
